b3e5ae1d35b1226ebf21d61b5498c65d2d72b53f
pw-wsb <101039960+pw-wsb@users.noreply.github.com>
2023-01-15 13:50:02 +0100

    aktualizacja kodu

--- generator_hasel.rb
+++ generator_hasel.rb
@@ -1,12 +1,11 @@
-def generate_password(length: 20, characters: [:lowercase,:uppercase,:digits,:special_chars])
-  characters_set = { lowercase: ('a'..'z').to_a,
-                     uppercase: ('A'..'Z').to_a,
-                     digits: (0..9).to_a,
-                     special_chars: %w{!@#%^&*()-_=+[]{}\/:;<>,.?}
-                   }
-  password_characters = characters_set.values_at(*characters).join
-  password = password_characters.split('').shuffle[0...length].join
-  puts password
+def generate_password
+  special_characters = %w{!@#%^&*()-_=+[]{}\/:;<>,.?}
+  lowercase_characters = ('a'..'z').to_a
+  uppercase_characters = ('A'..'Z').to_a
+  digits = (0..9).to_a
+  password = Array.new(7) { lowercase_characters.sample } + Array.new(7) { uppercase_characters.sample } + Array.new(7) { digits.sample }
+  password << special_characters.sample
+  puts "Oto twoje hasło: #{password.shuffle!.join}"
 end
 
-generate_password(characters: [:lowercase, :special_chars, :digits])
+generate_password
